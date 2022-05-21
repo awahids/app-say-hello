88f03030230dc64cf6df9717f42bdd45e51f10e4
awhds <awahid.safhadi@gmail.com>
2022-05-21 20:46:14 +0700

    belajar module

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/awahids/app-say-hello
+
+go 1.18
+
+require (
+	github.com/ProgrammerZamanNow/go-say-hello/v2 v2.0.0
+	github.com/awahids/go-say-hello/v2 v2.0.0
+)
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ProgrammerZamanNow/go-say-hello/v2"
+	"github.com/awahids/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("wahid"))
+	fmt.Println(go_say.SayHello("bebek"))
+}
